b54c97462e14e45596fcdf6fe6b93acb2548fa9d
Tuan Duong <dmtuan1995@gmail.com>
2018-04-14 17:30:22 +0700

    Basic Koa server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const router = new Router();
+router.get('/', async ({ request, response }, next) => {
+    response.body = 'Service is up and running';
+});
+router.get('/hello', async ({ request, response }, next) => {
+    response.body = 'Hello world';
+});
+
+const app = new Koa();
+app.use(router.routes());
+
+const server = http.createServer(app.callback());
+server.listen(8080, () => console.log('Server is listening on port', 8080));
